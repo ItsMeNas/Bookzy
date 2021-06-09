8562f956e66ef3f726f3760736d280c6053ad0f5
Nizam Shariff <36436660+ItsMeNas@users.noreply.github.com>
2021-06-10 00:26:06 +0300

    Update BookzyApplication.java

--- src/main/java/bookzy/com/Bookzy/BookzyApplication.java
+++ src/main/java/bookzy/com/Bookzy/BookzyApplication.java
@@ -2,8 +2,10 @@ package bookzy.com.Bookzy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"bookzy.com.Bookzy"})
 public class BookzyApplication {
 
 	public static void main(String[] args) {
