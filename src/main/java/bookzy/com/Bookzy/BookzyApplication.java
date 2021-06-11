ea1389e77f9bff74983d48a708d0b51a1381bf5a
Nizam Shariff <36436660+ItsMeNas@users.noreply.github.com>
2021-06-11 09:28:32 +0300

    Update BookzyApplication.java

--- src/main/java/bookzy/com/Bookzy/BookzyApplication.java
+++ src/main/java/bookzy/com/Bookzy/BookzyApplication.java
@@ -1,20 +1,40 @@
-package net.codejava;
- 
+package bookzy.com.Bookzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
+
+
+package com.javatpoint.controller;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RestController;  
+
+@SpringBootApplication
+@ComponentScan(basePackages={"bookzy.com.Bookzy"})
+
+@RequestMapping("/")
+
+public class BookzyApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(BookzyApplication.class, args);
+  SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);
+	}
  
-@RestController
-@EnableAutoConfiguration
-public class SpringBootHelloWorld {
- 
-    @RequestMapping("/")
-    String home() {
+ 	String home() {
         return "Hello World Spring Boot!";
     }
- 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(SpringBootHelloWorld.class, args);
-    }
- 
 }
+
+public class HelloWorldController   
+{  
+@RequestMapping("/")  
+public String hello()   
+{  
+return "Hello javaTpoint";  
+}  
+}  
