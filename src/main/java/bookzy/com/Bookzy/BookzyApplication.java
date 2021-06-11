7a26351c0ec2a3b768a608c9ed24fffa864a9e9a
Nizam Shariff <36436660+ItsMeNas@users.noreply.github.com>
2021-06-11 09:30:45 +0300

    Update BookzyApplication.java

--- src/main/java/bookzy/com/Bookzy/BookzyApplication.java
+++ src/main/java/bookzy/com/Bookzy/BookzyApplication.java
@@ -8,33 +8,13 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
-
-package com.javatpoint.controller;  
-import org.springframework.web.bind.annotation.RequestMapping;  
-import org.springframework.web.bind.annotation.RestController;  
-
 @SpringBootApplication
 @ComponentScan(basePackages={"bookzy.com.Bookzy"})
-
-@RequestMapping("/")
 
 public class BookzyApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(BookzyApplication.class, args);
-  SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);
+		SpringApplication.run(BookzyApplication.class, args);
 	}
- 
- 	String home() {
-        return "Hello World Spring Boot!";
-    }
-}
 
-public class HelloWorldController   
-{  
-@RequestMapping("/")  
-public String hello()   
-{  
-return "Hello javaTpoint";  
-}  
-}  
+}
