1631728c3fb2132e395df35961125a4e65cf8751
Nizam Shariff <36436660+ItsMeNas@users.noreply.github.com>
2021-06-11 08:54:33 +0300

    Update BookzyApplication.java

--- src/main/java/bookzy/com/Bookzy/BookzyApplication.java
+++ src/main/java/bookzy/com/Bookzy/BookzyApplication.java
@@ -4,9 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
 @SpringBootApplication
 @ComponentScan(basePackages={"bookzy.com.Bookzy"})
+
 @RequestMapping("/")
+
 public class BookzyApplication {
 	String home() {
         return "Hello World Spring Boot!";
